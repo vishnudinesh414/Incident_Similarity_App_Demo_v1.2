9a7655b81c89cd9b788fed26700f54d25d23a6c3
Vishnu Dinesh <vishnudinesh414@outlook.com>
2024-10-29 11:14:00 +0530

    version changes

--- backend/app.py
+++ backend/app.py
@@ -52,4 +52,5 @@ def get_clusters():
     })
 
 if __name__ == "__main__":
-    serve(app, host='127.0.0.1', port=5000)
+    logging.info("Server starting....")
+    serve(app, host='0.0.0.0', port=5000)
